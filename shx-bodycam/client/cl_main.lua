641ff50fdcf0cc515f87a1bcdecf6c5c23a777ad
Pablo Carmona <80336428+Pablosatorre@users.noreply.github.com>
2023-12-02 13:29:15 +0100

    Update cl_main.lua
    Added an animation to put the bodycam, and change the cloth to add a real bodycam

--- shx-bodycam/client/cl_main.lua
+++ shx-bodycam/client/cl_main.lua
@@ -2,6 +2,7 @@ local isAxonOn = false
 
 RegisterCommand("bodycam", function()
 	TriggerServerEvent("shx-bodycam/server/openBodycam")
+	PutClothes(Config.DrawableId, Config.TextureId)
 end)
 
 RegisterNetEvent("shx-bodycam/client/openEvent")
@@ -20,3 +21,15 @@ AddEventHandler("shx-bodycam/client/toggleOpen", function(finalname, dept, rank)
 	SendNUIMessage({show = true, type = "axon", name = finalname, dept = dept, rank = rank })
 	isAxonOn = true
 end)
+
+PutClothes = function(drawableId, textureId)
+    local playerPed = PlayerPedId()
+
+    while not HasAnimDictLoaded("clothingtie") do 
+        RequestAnimDict("clothingtie") 
+        Wait(100) 
+    end
+	
+    TaskPlayAnim(playerPed, "clothingtie", "try_tie_negative_a", 3.0, 3.0, 1200, 51, 0, false, false, false)
+    SetPedComponentVariation(playerPed, 9, drawableId, textureId, 0)
+end
