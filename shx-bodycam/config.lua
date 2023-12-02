25e93d7d8125bb12d73a4d8841835f8ab829f53a
Pablo Carmona <80336428+Pablosatorre@users.noreply.github.com>
2023-12-02 13:30:05 +0100

    Update config.lua

--- shx-bodycam/config.lua
+++ shx-bodycam/config.lua
@@ -2,6 +2,9 @@ Config = {}
 
 Config.Framework 	   = "qbox"  -- "qbcore" or "esx" or "qbox"
 
+Config.DrawableId = 5 
+Config.TextureId = 1 
+
 Config.axonDepartments = {       -- Job name Department
 	["police"] = "Los Santos Police Department",
 	["bcso"] = "Blaine County Sheriff Office",
